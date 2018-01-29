1dbd28116221c5ddb04940ba5b6493e88bc84de7
AnkitSiva <ankit.siva@gatech.edu>
2018-01-29 14:16:30 -0500

    My code is working now, time to add some whitespace to make the output look cleaner. Also need to fix the rotation.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -45,7 +45,7 @@ public class Person1 {
 
         String output = new String();
 
-        for (int i = input.length() - 1; i > -1; i++) {
+        for (int i = input.length() - 1; i > -1; i--) {
                 output += input.charAt((i + 2) % input.length());
         }
         
